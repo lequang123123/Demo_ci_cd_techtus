d7429aa7f8215385f4dd5c2f0794bc325de6fc04
quangle123123 <kuanglee1231@gmail.com>
2025-03-02 07:18:47 +0700

    Update route dom

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+// src/pages/Home.js
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Trang chủ</h1>
+            <Link to="/about">Đi đến About</Link>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/routes/Router.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "components/Home";
+import About from "components/About";
+import NotFound from "components/NotFound";
+
+const AppRouter = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default AppRouter;
